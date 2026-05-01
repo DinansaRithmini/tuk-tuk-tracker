52e2b42a8328d3367ccb124e077aa4c6aabf56b2
Thanuga-Rodrigo <dinarithmi123@gmail.com>
2026-05-01 15:30:03 +0530

    Fix port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 // Error Handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
